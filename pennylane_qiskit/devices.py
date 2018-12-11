090821507157553e267e0d5c2d93389a5197c4b4
Carsten Blank <blank@data-cybernetics.com>
2018-12-11 08:30:25 +0100

    Fixing the issue of there is a token from the environment but not from kwargs.

--- pennylane_qiskit/devices.py
+++ pennylane_qiskit/devices.py
@@ -314,6 +314,6 @@ class IbmQQiskitDevice(QiskitDevice):
         backend = kwargs.get('backend', 'ibmq_qasm_simulator')
         super().__init__(wires, backend=backend, shots=shots, **kwargs)
         self._provider = qiskit.IBMQ
-        if kwargs['ibmqx_token'] not in map(lambda e: e['token'], self._provider.active_accounts()):
+        if token not in map(lambda e: e['token'], self._provider.active_accounts()):
             self._provider.enable_account(token)
         self._capabilities['backend'] = [b.name() for b in self._provider.available_backends()]
