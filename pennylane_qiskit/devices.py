c87dd5c793d84f275743a4cc02bfa54375ae6419
Carsten Blank <blank@data-cybernetics.com>
2019-02-04 11:43:01 +0100

    Hm, this should work. Another attempt.

--- pennylane_qiskit/devices.py
+++ pennylane_qiskit/devices.py
@@ -100,7 +100,7 @@ QISKIT_OPERATION_MAP = {
 class QiskitDevice(Device):
     name = 'Qiskit PennyLane plugin'
     short_name = 'qiskit'
-    pennylane_requires = '0.1.0,0.2.0'
+    pennylane_requires = '<=0.2.0'
     version = '0.2.0'
     plugin_version = __version__
     author = 'Carsten Blank'
