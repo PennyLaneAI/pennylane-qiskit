0a20ca85673bc34ff72bffac91db329d870ddde7
antalszava <antalszava@gmail.com>
2019-10-28 20:00:58 -0400

    Initial outline of the QuantumCircuit converter

--- pennylane_qiskit/load.py
+++ pennylane_qiskit/load.py
@@ -29,6 +29,8 @@ Code details
 ~~~~~~~~~~~~
 """
 from qiskit import QuantumCircuit
+from pennylane_qiskit.qiskit_device import QISKIT_OPERATION_MAP
+import pennylane.operation as ops
 
 # pylint: disable=too-many-instance-attributes
 
@@ -43,67 +45,57 @@ class QuantumCircuitToTemplateConverter:
         # unsupported gates which are essential to the quantum algorithm should instead raise an error
 
 
+_operation_map = QISKIT_OPERATION_MAP
+inv_map = {v.__name__: k for k, v in _operation_map.items()}
+a = 3
 
-_operation_map = {
-    'BasisState': None,
-    'QubitStateVector': None,
-    'QubitUnitary': unitary,
-    'PauliX': X,
-    'PauliY': Y,
-    'PauliZ': Z,
-    'h': 'Hadamard',
-    'S': S,
-    'T': T,
-    'CNOT': CNOT,
-    'SWAP': SWAP,
-    'CSWAP':CSWAP,
-    'CZ': CZ,
-    'PhaseShift': Rphi,
-    'RX': Rotx,
-    'RY': Roty,
-    'RZ': Rotz,
-    'Rot': Rot3,
-    'CRX': CRotx,
-    'CRY': CRoty,
-    'CRZ': CRotz,
-    'CRot': CRot3
-    'unitary': 'Operation'
-}
-
-_observable_map = {
-    'PauliX': X,
-    'PauliY': Y,
-    'PauliZ': Z,
-    'Hadamard': H,
-    'Hermitian': hermitian,
-    'Identity': identity
-}
 
 # Defining the load function
-def qiskit_to_pennylane(quantum_circuit: QuantumCircuit):
+def qiskit_to_pennylane(quantum_circuit: QuantumCircuit, params: dict = None):
 
     if not isinstance(quantum_circuit, QuantumCircuit):
         raise ValueError("The circuit {} is not a valid Qiskit QuantumCircuit.".format(quantum_circuit))
 
-    def _function(params, wires):
+    operator_queue = []
 
-        # Processing the dictionary of parameters passed
-        circuit_with_parameters = quantum_circuit.bind_parameters(params)
-        for op in quantum_circuit.data:
+    # TODO:
+    # Check if parameters are needed -> if so, bind them
+    # Check if parameters are needed -> if not passed, raise ERROR
+    # Check if parameters are not needed -> if not passed, go on
+    # Check if parameters are not needed -> if passed, , raise WARNING
 
-            operator_name = op[0].name
+    if len(quantum_circuit.parameters) > 0:
+        if params is not None:
+            quantum_circuit = quantum_circuit.bind_parameters(params)
+        else:
+            raise ValueError("Parameters required for circuit {}.".format(quantum_circuit.name))
+    else:
+        if params is not None:
+            print("Parameters were passed although the  circuit {} does not require them.".format(quantum_circuit.name))
 
-            # First item of the list contains the gate
-            pennylane_operator = _operation_map[operator_name]
+    # Processing the dictionary of parameters passed
+    for op in quantum_circuit.data:
 
+        #TODO:
+        # What if multiple quantum registers are specified
+        # Indexing a Qubit object to obtain a wire
 
-            # Second the wires it acts on
+        wires = [qubit.index for qubit in op[1]]
+        operation = getattr(ops, op[0].name)
 
+        # Call each operator with the specified parameters
+        parameters = op[0].params
 
+        if not parameters:
+            operation(wires=wires)
+        else:
+            operation(*parameters, wires=wires)
 
-            # convert operation to PennyLane operation
-            # pass parameter dictionary & wire
-    return _function
+    return operator_queue
+
+def load(quantum_circuit: QuantumCircuit):
+    queue = qml.converter()
+    template = qiskit_to_pennylane(quantum_circuit)
 
 
     #[docs]    @staticmethod
@@ -117,3 +109,20 @@ def qiskit_to_pennylane(quantum_circuit: QuantumCircuit):
     #    """
     #    qasm = Qasm(filename=path)
     #    return _circuit_from_qasm(qasm)
+
+    def _function(params, wires):
+
+            pennylane_operator = inv_map[operator_name]
+            result = getattr(qml.ops, pennylane_operator)()
+            # First item of the list contains the gate
+
+            queue =
+
+            # Second the wires it acts on
+
+
+
+            # convert operation to PennyLane operation
+            # pass parameter dictionary & wire
+    return _function
+
