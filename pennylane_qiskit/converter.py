ca976455ef79dcdc0e2835fb24ae665969709622
antalszava <antalszava@gmail.com>
2019-10-31 20:49:22 +0100

    Update pennylane_qiskit/converter.py
    Co-Authored-By: Josh Izaac <josh146@gmail.com>

--- pennylane_qiskit/converter.py
+++ pennylane_qiskit/converter.py
@@ -53,7 +53,7 @@ def check_parameter_bound(param):
 
 
 def load(quantum_circuit: QuantumCircuit):
-    """Returns a PennyLane template created based on the input qiskit.QuantumCircuit.
+    """Returns a PennyLane template created based on the input QuantumCircuit or QASM string.
     Warnings are created for each of the QuantumCircuit instructions that were
     not incorporated in the PennyLane template.
 
