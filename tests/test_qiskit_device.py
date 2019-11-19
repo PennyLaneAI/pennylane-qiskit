1223960f279215d7bee64489cc1c05cb50a9d8fd
antalszava <antalszava@gmail.com>
2019-11-19 10:57:08 -0500

    Add tests for the variance case

--- tests/test_qiskit_device.py
+++ tests/test_qiskit_device.py
@@ -45,7 +45,7 @@ class TestAnalyticWarningHWSimulator:
 
     def test_warning_raised_for_hardware_backend_analytic_expval(self, hardware_backend, recorder):
         """Tests that a warning is raised if the analytic attribute is true on
-            hardware simulators"""
+            hardware simulators when calculating the expectation"""
 
         dev = qml.device("qiskit.basicaer", backend=hardware_backend, wires=2)
 
@@ -66,7 +66,7 @@ class TestAnalyticWarningHWSimulator:
 
     def test_no_warning_raised_for_software_backend_analytic_expval(self, statevector_backend, recorder, recwarn):
         """Tests that no warning is raised if the analytic attribute is true on
-            statevector simulators"""
+            statevector simulators when calculating the expectation"""
 
         dev = qml.device("qiskit.basicaer", backend=statevector_backend, wires=2)
 
@@ -74,6 +74,43 @@ class TestAnalyticWarningHWSimulator:
         def circuit():
             qml.Hadamard(wires=0)
             return qml.expval(qml.PauliZ(0))
+
+        circuit()
+
+        # check that no warnings were raised
+        assert len(recwarn) == 0
+
+    def test_warning_raised_for_hardware_backend_analytic_var(self, hardware_backend, recorder):
+        """Tests that a warning is raised if the analytic attribute is true on
+            hardware simulators when calculating the variance"""
+
+        dev = qml.device("qiskit.basicaer", backend=hardware_backend, wires=2)
+
+        @qml.qnode(dev)
+        def circuit():
+            qml.Hadamard(wires=0)
+            return qml.var(qml.PauliZ(0))
+
+        with pytest.warns(UserWarning) as record:
+            circuit()
+
+        # check that only one warning was raised
+        assert len(record) == 1
+        # check that the message matches
+        assert record[0].message.args[0] == "The analytic calculation of expectations and variances "\
+                                            "is only supported on statevector backends, not on the {}. "\
+                                            "The obtained result is based on sampling.".format(dev.backend)
+
+    def test_no_warning_raised_for_software_backend_analytic_var(self, statevector_backend, recorder, recwarn):
+        """Tests that no warning is raised if the analytic attribute is true on
+            statevector simulators when calculating the variance"""
+
+        dev = qml.device("qiskit.basicaer", backend=statevector_backend, wires=2)
+
+        @qml.qnode(dev)
+        def circuit():
+            qml.Hadamard(wires=0)
+            return qml.var(qml.PauliZ(0))
 
         circuit()
 
