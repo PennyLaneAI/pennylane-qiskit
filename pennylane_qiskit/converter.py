5662b53a50869007429a28914b9373c622d08e1d
Theodor <theodor@xanadu.ai>
2020-07-24 18:32:11 -0400

    Update pennylane_qiskit/converter.py
    Co-authored-by: antalszava <antalszava@gmail.com>

--- pennylane_qiskit/converter.py
+++ pennylane_qiskit/converter.py
@@ -175,7 +175,7 @@ def load(quantum_circuit: QuantumCircuit):
                 # variable from the var_ref_map dictionary.
 
                 parameters = []
-                for p in op[0].data:
+                for p in op[0].params:
 
                     _check_parameter_bound(p, var_ref_map)
 
