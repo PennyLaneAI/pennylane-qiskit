a73662d96368346bd51336d409e69c70ec8a6bc6
Jay Soni <jbsoni@uwaterloo.ca>
2021-10-20 09:32:39 -0400

    added docstrings, removed empty lines

--- vb_script.py
+++ vb_script.py
@@ -1,15 +1,32 @@
+# This script handles the logic to update the changelog and version.py file
+# for the automated version bumps
+
 import argparse
 import pennylane as qml
-pl_version = '"' + qml.version() + '"\n' 
+pl_version = '"' + qml.version() + '"\n'
 
 
 def bump_version(version_line, pre_release):
+    """ A helper function which takes the current version string and
+    replaces it with the bumped version depending on the pre/post
+    release flag.
+
+    Args:
+         version_line (string): The string containing the current
+            version of the plugin.
+         pre_release (bool): A flag which determines if this is a
+            pre-release or post-release version bump.
+
+    Returns:
+        resultant_line (string): A string of the same form as the version line
+        with the version number replaced with the bumped version.
+        bumped_version (string): The bumped version string.
+    """
     data = version_line.split(" ")
     curr_version = data[-1]
 
     if pre_release:
         bumped_version = pl_version.replace("-dev", "")  # get current Pennylane version then remove -dev
-#         bumped_version = curr_version.replace("-dev", "")  # remove -dev
     else:
         split_version = curr_version.split(".")  # "0.17.0" --> ["0,17,0"]
         split_version[1] = str(int(split_version[1]) + 1)  # take middle value and cast as int and bump it by 1
@@ -21,10 +38,20 @@ def bump_version(version_line, pre_release):
 
 
 def update_version_file(path, pre_release=True):
-    with open(path, 'r') as f:
+    """ Updates the __version__ attribute in a specific version file.
+
+    Args:
+        path (str): The path to the version file.
+        pre_release (bool): A flag which determines if this is a
+            pre-release or post-release version bump.
+
+    Return:
+        new_version (str): The bumped version string.
+    """
+    with open(path, 'r', encoding="utf8") as f:
         lines = f.readlines()
 
-    with open(path, 'w') as f:
+    with open(path, 'w', encoding="utf8") as f:
         for line in lines:
             if "__version__" in line.split(' '):
                 new_line, new_version = bump_version(line, pre_release)
@@ -35,6 +62,15 @@ def update_version_file(path, pre_release=True):
 
 
 def remove_empty_headers(lines):
+    """ Takes a paragraph (list of strings) and removes sections which are empty.
+    Where a section begins with a header (### Header_Title).
+
+    Args:
+        lines (list of strings): the paragraph containing the sections.
+
+    Returns:
+        cleaned_lines (list of strings): The paragraph with empty sections removed.
+    """
     cleaned_lines = []
     pntr1 = 0
 
@@ -63,7 +99,18 @@ def remove_empty_headers(lines):
 
 
 def update_changelog(path, new_version, pre_release=True):
-    with open(path, 'r') as f:
+    """ Updates the Changelog file depending on the pre_release flag.
+
+    Args:
+        path (str): The path to the changelog file.
+        new_version (str): The bumped version string.
+        pre_release (bool): A flag which determines if this is a
+            pre-release or post-release version bump.
+
+    Returns:
+        None
+    """
+    with open(path, 'r', encoding="utf8") as f:
         lines = f.readlines()
         end_of_section_index = 0
         for index, line in enumerate(lines):
@@ -71,9 +118,9 @@ def update_changelog(path, new_version, pre_release=True):
                 end_of_section_index = index
                 break
 
-    with open(path, 'w') as f:
+    with open(path, 'w', encoding="utf8") as f:
         if not pre_release:  # post_release append template to top of the changelog
-            with open("./changelog_template.txt", 'r') as template_f:
+            with open("./changelog_template.txt", 'r', encoding="utf8") as template_f:
                 template_lines = template_f.readlines()
                 template_lines[0] = template_lines[0].replace('x.x.x-dev', new_version)
                 f.writelines(template_lines)
@@ -97,12 +144,6 @@ def update_changelog(path, new_version, pre_release=True):
     return
 
 
-def main(version_file_path, changelog_file_path, pre_release=True):
-    new_version = update_version_file(version_file_path, pre_release)
-    update_changelog(changelog_file_path, new_version, pre_release)
-    return
-
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--version_path", type=str, required=True, help="Path to the _version.py file")
@@ -113,4 +154,5 @@ if __name__ == "__main__":
                         help="True if this is a pre-release version bump, False if it is post release")
 
     args = parser.parse_args()
-    main(args.version_path, args.changelog_path, args.release_status)
+    new_version = update_version_file(args.version_path, args.release_status)
+    update_changelog(args.changelog_path, new_version, args.release_status)
