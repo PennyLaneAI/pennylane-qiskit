a0990163204b5404f3fe25da4b310d4e1661824f
Romain Moyard <rmoyard@gmail.com>
2021-11-25 14:11:33 -0500

    Test Sampler.

--- tests/test_runtime.py
+++ tests/test_runtime.py
@@ -87,9 +87,52 @@ class TestSampler:
         dev = IBMQSamplerDevice(wires=1)
         assert dev.provider.credentials.is_ibmq()
 
-    def test_simple_circuit(self):
+    def test_short_name(self):
+        dev = qml.device("qiskit.ibmq.sampler", wires=1)
+        return dev.provider.credentials.is_ibmq()
 
-        return True
+    @pytest.mark.parametrize("shots", [1000])
+    def test_simple_circuit(self, token, tol, shots):
+        """Test executing a simple circuit submitted to IBMQ."""
+        IBMQ.enable_account(token)
+        dev = IBMQSamplerDevice(wires=2, backend="ibmq_qasm_simulator", shots=shots)
+
+        @qml.qnode(dev)
+        def circuit(theta, phi):
+            qml.RX(theta, wires=0)
+            qml.RX(phi, wires=1)
+            qml.CNOT(wires=[0, 1])
+            return qml.expval(qml.PauliZ(0)), qml.expval(qml.PauliZ(1))
+
+        theta = 0.432
+        phi = 0.123
+
+        res = circuit(theta, phi)
+        expected = np.array([np.cos(theta), np.cos(theta) * np.cos(phi)])
+        assert np.allclose(res, expected, **tol)
+
+    @pytest.mark.parametrize("shots", [1000])
+    def test_batch_circuits(self, token, tol, shots):
+
+        IBMQ.enable_account(token)
+        dev = IBMQSamplerDevice(wires=2, backend="ibmq_qasm_simulator", shots=shots)
+
+        # Batch the input parameters
+        batch_dim = 3
+        a = np.linspace(0, 0.543, batch_dim)
+        b = np.linspace(0, 0.123, batch_dim)
+        c = np.linspace(0, 0.987, batch_dim)
+
+        @qml.batch_params
+        @qml.qnode(dev)
+        def circuit(x, y, z):
+            """Reference QNode"""
+            qml.PauliX(0)
+            qml.Hadamard(wires=0)
+            qml.Rot(x, y, z, wires=0)
+            return qml.expval(qml.PauliZ(0))
+
+        assert np.allclose(circuit(a, b, c), np.cos(a) * np.sin(b), **tol)
 
 class TestCustomVQE:
 
