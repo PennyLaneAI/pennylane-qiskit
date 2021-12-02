132df4b2740a3a9ccc32114d7608f3d5dcbaba36
Antal Szava <antalszava@gmail.com>
2021-12-02 13:54:22 -0500

    check type

--- tests/test_qiskit_device.py
+++ tests/test_qiskit_device.py
@@ -155,8 +155,10 @@ class TestBatchExecution:
         tape2_expected = dev.execute(self.tape2)
 
         assert len(res) == 2
+        assert isinstance(res[0], np.ndarray)
         assert np.allclose(res[0], tape1_expected, atol=0)
 
+        assert isinstance(res[1], np.ndarray)
         assert np.allclose(res[1], tape2_expected, atol=0)
 
     def test_result_empty_tape(self, device, tol):
