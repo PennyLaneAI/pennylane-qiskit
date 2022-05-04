ac5b3a1f212c379ba8346a30c0647ef5beb5cb80
Romain Moyard <rmoyard@gmail.com>
2022-05-04 11:56:52 -0400

    update

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ with open("README.rst", "r") as fh:
 requirements = [
     "qiskit>=0.32",
     "mthree>=0.17",
-    "pennylane>=0.22",
+    "pennylane @ git+https://github.com/PennyLaneAI/pennylane.git",
     "numpy",
     "networkx>=2.2",
 ]
