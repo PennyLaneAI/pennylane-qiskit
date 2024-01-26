33e4b3b46ab4ea5fe8eda1592b7d488fe295bad9
lillian542 <Lillian.frederiksen@xanadu.ai>
2024-01-26 12:33:45 -0500

    fix wire order bug for Estimator returns

--- pennylane_qiskit/converter.py
+++ pennylane_qiskit/converter.py
@@ -408,6 +408,9 @@ def mp_to_pauli(mp, register_size):
     pauli_string = ["I"] * register_size
     pauli_string[mp.wires[0]] = observables[mp.obs.name]
 
+    # Qiskit orders wires in the opposite direction compared to PL
+    pauli_string.reverse()
+
     pauli_string = ('').join(pauli_string)
 
     return SparsePauliOp(pauli_string)
