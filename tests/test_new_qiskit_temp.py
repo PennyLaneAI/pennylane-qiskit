8d7203a6c46d43f7fd17bc4d5891f499e77957bd
lillian542 <Lillian.frederiksen@xanadu.ai>
2024-02-15 22:35:05 -0500

    mock for codecov

--- tests/test_new_qiskit_temp.py
+++ tests/test_new_qiskit_temp.py
@@ -3,16 +3,15 @@ import pennylane as qml
 import qiskit
 
 
-def test_error_is_raised_if_initalizing_device():
+def test_error_is_raised_if_initalizing_device(monkeypatch):
     """Test that when Qiskit 1.0 is installed, an error is raised if you try
     to initialize a device. This is a temporary test and will be removed along
     with the error one everything is compatible with 1.0"""
     if qiskit.__version__ != "1.0.0":
-        pass
+        monkeypatch.setattr(qiskit, "__version__", "1.0.0")
 
-    else:
-        with pytest.raises(
-            RuntimeError,
-            match="The devices in the PennyLane Qiskit plugin are currently only compatible with version of Qiskit below 0.46",
-        ):
-            qml.device("qiskit.aer", wires=2)
+    with pytest.raises(
+        RuntimeError,
+        match="The devices in the PennyLane Qiskit plugin are currently only compatible with version of Qiskit below 0.46",
+    ):
+        qml.device("qiskit.aer", wires=2)
