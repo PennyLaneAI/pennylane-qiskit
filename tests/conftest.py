3910c33416a5edca14e0fde4937d798667452d24
Utkarsh <utkarshazad98@gmail.com>
2024-04-28 21:29:36 -0400

    Update tests/conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 r"""
-This module contains come configuaration for PennyLane IBMQ devices.
+This module contains some configuration for PennyLane IBMQ devices.
 """
 
 import os
