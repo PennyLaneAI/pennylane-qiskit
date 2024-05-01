8a77bb746f10473eab64037799779d9901813fe5
Austin Huang <gengminghuang@gmail.com>
2024-05-01 09:29:29 -0400

    black linter

--- tests/conftest.py
+++ tests/conftest.py
@@ -22,7 +22,7 @@ import qiskit
 
 import pennylane as qml
 from semantic_version import Version
-from qiskit_ibm_provider import IBMProvider, IBMProviderValueError
+from qiskit_ibm_provider import IBMProvider
 from pennylane_qiskit import AerDevice, BasicAerDevice, BasicSimulatorDevice
 
 # pylint: disable=protected-access, unused-argument, redefined-outer-name
@@ -58,8 +58,8 @@ else:
     ]
 
 
-@pytest.fixture
 # pylint: disable=broad-except
+@pytest.fixture
 def skip_if_no_account():
     t = os.getenv("IBMQX_TOKEN", None)
     try:
