96ddb61815351e39dcc0fa8c3a4bd3e709f43e37
Austin Huang <gengminghuang@gmail.com>
2024-05-17 14:30:49 -0400

    removed devices

--- tests/conftest.py
+++ tests/conftest.py
@@ -23,7 +23,7 @@ import qiskit
 import pennylane as qml
 from semantic_version import Version
 from qiskit_ibm_provider import IBMProvider
-from pennylane_qiskit import AerDevice, BasicAerDevice, BasicSimulatorDevice
+from pennylane_qiskit import AerDevice, BasicSimulatorDevice
 
 # pylint: disable=protected-access, unused-argument, redefined-outer-name
 
@@ -40,22 +40,14 @@ U2 = np.array([[0, 1, 1, 1], [1, 0, 1, -1], [1, -1, 0, 1], [1, 1, -1, 0]]) / np.
 A = np.array([[1.02789352, 1.61296440 - 0.3498192j], [1.61296440 + 0.3498192j, 1.23920938 + 0j]])
 
 
-if Version(qiskit.__version__) < Version("1.0.0"):
-    test_devices = [AerDevice, BasicAerDevice]
-    hw_backends = ["qasm_simulator", "aer_simulator"]
-    state_backends = [
-        "statevector_simulator",
-        "unitary_simulator",
-    ]
-else:
-    test_devices = [AerDevice, BasicSimulatorDevice]
-    hw_backends = ["qasm_simulator", "aer_simulator", "basic_simulator"]
-    state_backends = [
-        "statevector_simulator",
-        "unitary_simulator",
-        "aer_simulator_statevector",
-        "aer_simulator_unitary",
-    ]
+test_devices = [AerDevice, BasicSimulatorDevice]
+hw_backends = ["qasm_simulator", "aer_simulator", "basic_simulator"]
+state_backends = [
+    "statevector_simulator",
+    "unitary_simulator",
+    "aer_simulator_statevector",
+    "aer_simulator_unitary",
+]
 
 
 @pytest.fixture
