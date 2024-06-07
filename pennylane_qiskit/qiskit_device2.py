d66f4418a11f139fa49091c9ff97f183125a13a7
Austin Huang <gengminghuang@gmail.com>
2024-06-07 09:35:07 -0400

    clean up

--- pennylane_qiskit/qiskit_device2.py
+++ pennylane_qiskit/qiskit_device2.py
@@ -263,7 +263,9 @@ class QiskitDevice2(Device):
             raise ValueError(f"Backend '{backend}' supports maximum {available_qubits} wires")
 
         self.reset()
-       self._transpile_args, self._kwargs = self._process_kwargs(kwargs)  # processes kwargs and separates transpilation arguments to dev._transpile_args
+        self._kwargs, self._transpile_args = self._process_kwargs(
+            self._kwargs
+        )  # processes kwargs and separates transpilation arguments to dev._transpile_args
 
     @property
     def backend(self):
@@ -369,37 +371,39 @@ class QiskitDevice2(Device):
 
         return transform_program, config
 
-    def _process_kwargs(self):
+    def _process_kwargs(self, kwargs):
         """Combine the settings defined in options and the settings passed as kwargs, with
         the definition in options taking precedence if there is conflicting information.
         Arguments related to transpilation are separated and saved in `dev._transpile_args`."""
 
-        if "noise_model" in self._kwargs:
-            noise_model = self._kwargs.pop("noise_model")
+        if "noise_model" in kwargs:
+            noise_model = kwargs.pop("noise_model")
             self.backend.set_options(noise_model=noise_model)
 
-        if "options" in self._kwargs:
-            for key, val in self._kwargs.pop("options").items():
-                if key in self._kwargs:
+        if "options" in kwargs:
+            for key, val in kwargs.pop("options").items():
+                if key in kwargs:
                     warnings.warn(
                         "An overlap between what was passed in via options and what was passed in via kwargs was found."
                         f"The value set in options {key}={val} will be used."
                     )
-                self._kwargs[key] = val
+                kwargs[key] = val
 
-        shots = self._kwargs.pop("shots")
+        shots = kwargs.pop("shots")
 
-        if "default_shots" in self._kwargs:
+        if "default_shots" in kwargs:
             warnings.warn(
                 f"default_shots was found in the keyword arguments, but it is not supported by {self.name}"
                 "Please use the `shots` keyword argument instead. The number of shots "
                 f"{shots} will be used instead."
             )
-        self._kwargs["default_shots"] = shots
+        kwargs["default_shots"] = shots
 
-        self._transpile_args = self.get_transpile_args()
+        kwargs, transpile_args = self.get_transpile_args(kwargs)
 
-    def get_transpile_args(self, kwargs): --> _transpile_args
+        return kwargs, transpile_args
+
+    def get_transpile_args(self, kwargs):
         """The transpile argument setter.
 
         Keyword Args:
@@ -409,13 +413,11 @@ class QiskitDevice2(Device):
 
         transpile_sig = inspect.signature(transpile).parameters
 
-        transpile_args = {
-            arg: self._kwargs.pop(arg) for arg in transpile_sig if arg in self._kwargs
-        }
+        transpile_args = {arg: kwargs.pop(arg) for arg in transpile_sig if arg in kwargs}
         transpile_args.pop("circuits", None)
         transpile_args.pop("backend", None)
 
-        return transpile_args
+        return kwargs, transpile_args
 
     def compile_circuits(self, circuits):
         r"""Compiles multiple circuits one after the other.
