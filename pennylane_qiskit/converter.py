fd8ceec4598a1970cdbe46badf19eae32a3a1078
Austin Huang <gengminghuang@gmail.com>
2024-06-20 16:54:20 -0400

    pylint

--- pennylane_qiskit/converter.py
+++ pennylane_qiskit/converter.py
@@ -35,8 +35,8 @@ from sympy import lambdify
 
 import pennylane as qml
 import pennylane.ops as pennylane_ops
-from pennylane_qiskit.qiskit_device import QISKIT_OPERATION_MAP
 from pennylane.tape.tape import rotations_and_diagonal_measurements
+from pennylane_qiskit.qiskit_device import QISKIT_OPERATION_MAP
 
 inv_map = {v.__name__: k for k, v in QISKIT_OPERATION_MAP.items()}
 
