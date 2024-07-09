79a058a5912696c963aa50e62932ada9477bce4a
Austin Huang <gengminghuang@gmail.com>
2024-07-09 14:01:35 -0400

    black

--- pennylane_qiskit/qiskit_device2.py
+++ pennylane_qiskit/qiskit_device2.py
@@ -52,7 +52,7 @@ from pennylane.devices.modifiers.simulator_tracking import (
     _track_execute_and_compute_jvp,
     _track_execute_and_compute_vjp,
     _track_compute_vjp,
-    _track_compute_jvp
+    _track_compute_jvp,
 )
 from pennylane.devices.qubit.sampling import get_num_shots_and_executions
 from ._version import __version__
