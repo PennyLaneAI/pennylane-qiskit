36671372ecc8a45726088a85c1314356b6333cef
Pietropaolo Frisoni <pietropfrisoni@gmail.com>
2025-03-05 09:59:15 -0500

    `setuptools` issue

--- pennylane_qiskit/_version.py
+++ pennylane_qiskit/_version.py
@@ -13,7 +13,7 @@
 # limitations under the License.
 
 """Version information.
-   Version number (major.minor.patch[-label])
+Version number (major.minor.patch[-label])
 """
 
 __version__ = "0.41.0-dev"
