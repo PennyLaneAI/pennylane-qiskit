6730ae3b201d1d42548f0e62a841e0592e801e8a
andrijapau <andrija.paurevic@xanadu.ai>
2025-10-22 15:47:16 -0400

    remove upper bound

--- setup.py
+++ setup.py
@@ -24,8 +24,8 @@ with open("README.rst", encoding="utf-8") as fh:
 
 requirements = [
     "qiskit>=2.0",
-    "qiskit-aer~=0.17.1",
-    "qiskit-ibm-runtime~=0.41.1",
+    "qiskit-aer>=0.17.1",
+    "qiskit-ibm-runtime>=0.41.1",
     "pennylane>=0.43",
     "sympy",
     "networkx>=2.2",
