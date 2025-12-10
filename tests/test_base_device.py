74f60cdf1f15fedcff6aab87f2b8042d5887b7a6
runora95 <59572091+runora95@users.noreply.github.com>
2025-12-10 17:37:46 -0500

    Upgrade to sphinx 8.1  (#676)
    * Pin Sphinx to 8.1.0
    
    * Update doc/requirements.txt
    
    Co-authored-by: Andrija Paurevic <46359773+andrijapau@users.noreply.github.com>
    
    * fix test (#675)
    
    * fix test
    
    * black
    
    * once again...
    
    * debug
    
    * reformat
    
    ---------
    
    Co-authored-by: Andrija Paurevic <46359773+andrijapau@users.noreply.github.com>
    Co-authored-by: Yushao Chen (Jerry) <chenys13@outlook.com>

--- tests/test_base_device.py
+++ tests/test_base_device.py
@@ -753,10 +753,7 @@ class TestTrackerFunctionality:
         with dev.tracker:
             pl_out = dev.execute(tape)
 
-        assert (
-            qiskit_dev.tracker.history["resources"][0].shots
-            == dev.tracker.history["resources"][0].shots
-        )
+        assert qiskit_dev.tracker.history["shots"] == dev.tracker.history["shots"]
         assert np.allclose(pl_out, qiskit_out, atol=0.1)
         assert np.allclose(
             qiskit_dev.tracker.history["results"],
