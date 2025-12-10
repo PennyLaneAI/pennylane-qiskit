0794500252abab331623b042b80a24414850ab96
JerryChen97 <chenys13@outlook.com>
2025-12-10 15:18:34 -0500

    debug

--- tests/test_base_device.py
+++ tests/test_base_device.py
@@ -754,7 +754,8 @@ class TestTrackerFunctionality:
             pl_out = dev.execute(tape)
 
         assert (
-            qiskit_dev.tracker.history["resources"].shots == dev.tracker.history["resources"].shots
+            qiskit_dev.tracker.history["shots"]
+            == dev.tracker.history["shots"]
         )
         assert np.allclose(pl_out, qiskit_out, atol=0.1)
         assert np.allclose(
